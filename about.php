a31ad57cd4e51dc2fa7e068c8ea78dd5beeb8699
Slobodan Lohja <slohja@xpagesbeast.com>
2018-04-08 01:13:14 -0500

    Added placeholder pages and updated navigation to launch placeholder pages

---
+++ about.php
@@ -0,0 +1,24 @@
+<?php # Script 3.7 - index.php #2
+
+// This function outputs theoretical HTML
+// for adding ads to a Web page.
+function create_ad() {
+	echo '<p class="ad">This is an annoying ad! This is an annoying ad! This is an annoying ad! This is an annoying ad!</p>';
+} // End of the function definition.
+
+$page_title = 'Welcome to this Site!';
+include ('includes/header.html');
+
+// Call the function:
+//create_ad();
+?>
+    <div class="container">
+        <h1>About Us</h1>
+    </div>
+<?php
+
+// Call the function again:
+//create_ad();
+
+include ('includes/footer.html');
+?>
