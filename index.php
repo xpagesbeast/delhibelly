786d7c680ad52229556b12c281382d228db41d68
Steve Lohja <slohja@CanalBarge.Net>
2018-04-08 01:01:08 -0500

    Landing Pages for the site

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php # Script 3.7 - index.php #2
+
+// This function outputs theoretical HTML
+// for adding ads to a Web page.
+function create_ad() {
+	echo '<p class="ad">This is an annoying ad! This is an annoying ad! This is an annoying ad! This is an annoying ad!</p>';
+} // End of the function definition.
+
+$page_title = 'Welcome to this Site!';
+include ('includes/header.html');
+
+// Call the function:
+//create_ad();
+?>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="/group6/images/indianchat2.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="/group6/images/indianchat2.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="/group6/images/indianchat2.jpg" alt="Third slide">
+            </div>
+        </div>
+    </div>
+<?php
+
+// Call the function again:
+//create_ad();
+
+include ('includes/footer.html');
+?>
